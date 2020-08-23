524e4767aef05de134ccac6667174a7178bb0094
junj2ejj <junj2ejj@gmail.com>
2020-08-23 19:57:32 +0900

    test

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 import setuptools
+from os import path
 
-long_description = ''
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+DATA_FILES = ['img/new_loss.png']
+
 
 install_requires = [
     'numpy',
     'scikit-learn',
     'tqdm',
-    #'torch >= 1.6.0', # todo torch-gpu
+    #'torch',
     #'torchvision',
-]
+    ],
 
-extras_requires = None
+
 
 setuptools.setup(
     name="ptranking",
-    version="0.1",
+    version="0.3",
     author="II-Research",
-    author_email="yuhaitao@slis.tsukuba.ac.jp",
+    author_email="example@example.com",
     description="A library of scalable and extendable implementations of typical learning-to-rank methods based on PyTorch.",
     license="MIT License",
     keywords=['Learning-to-rank', 'PyTorch'],
@@ -30,8 +36,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=install_requires,
-    extras_require=extras_requires
+    install_requires = install_requires,
+    data_files = DATA_FILES
 )
-
-#todo package_data
